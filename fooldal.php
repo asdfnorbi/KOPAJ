d757c32e753cf5e0ff7e663ed54a01634b87213f
tamaskelemen <tamaskelemen.kt@gmail.com>
2016-11-15 23:03:04 +0100

    fooldal really added

---
+++ fooldal.php
@@ -0,0 +1,76 @@
+    <b><h2>Arsenal FC</h2>
+<br>
+<br>
+Az Arsenal Football Club (röviden Arsenal, The Arsenal vagy az Ágyúsok) angol profi labdarúgócsapat Holloway-ben, Észak-Londonban. A Premier League-ben játszanak, és az egyik legsikeresebb klub az angol labdarúgásban 13 First Division és Premier League címmel és 12 FA Kupával.
+<br>
+<br>
+Az Arsenalt 1886-ban alapították, viszont elsõ fontosabb trófeáikat az 1930-as években nyerték, öt League Championship címet és két FA Kupát. Egy sikertelen idõszak után (a háború utáni években) a második olyan klub volt, amelynek sikerült megnyernie a duplát (az 1970–71-es szezonban).
+<br>
+<br>
+Az elkövetkezendõ húsz év folyamán az egyik legsikeresebb angol csapat lett, ebben az idõszakban az Arsenal megnyert két Duplát, a Premier League-et veretlenül, valamint az elsõ londoni klub volt, amely elérte az UEFA Bajnokok Ligája döntõjét.
+<p id="demo"></p>
+
+<script >
+
+var day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "vasarnap";
+        break;
+    case 1:
+        day = "hetfo";
+        break;
+    case 2:
+        day = "kedd";
+        break;
+    case 3:
+        day = "szerda";
+        break;
+    case 4:
+        day = "csutortok";
+        break;
+    case 5:
+        day = "pentek";
+        break;
+    case  6:
+        day = "szombat";
+        break;
+}
+document.getElementById("demo").innerHTML = "Mai nap: " + day;
+
+</script>
+<div id="tablazat">
+Legsikeresebb angol labdarugo csapatok a megnyert kupak szama alapjan:
+<table style="width:40%">
+  <tr>
+    <td>Manchester United</td>
+    <td>62</td>      
+    
+  </tr>
+  <tr>
+    <td>Liverpool</td>
+    <td>60</td>        
+    
+  </tr>
+  <tr>/
+    <td><b>ARSENAL</b></td>
+    <td>43</td>        
+    
+  </tr>
+  <tr>
+    <td>Chelsea</td>
+    <td>28</td>        
+    
+  </tr>
+  <tr>
+    <td>Aston Villa</td>
+    <td>24</td>        
+    
+  </tr>
+  <tr>
+    <td>Tottenham</td>
+    <td>24</td>        
+    
+  </tr>
+</table>
+</div>
